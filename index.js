eb3c2ff884edee202475e447a8b72bc2f8f433b9
adao74 <andreaminghweidao@hotmail.com>
2024-04-24 20:32:00 -0500

    add index.js and update  html file

---
+++ index.js
@@ -0,0 +1,44 @@
+// We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
+
+let arrayOfTodos = [
+    {
+    "userId": 14,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+},
+{
+    "userId": 20,
+    "id": 2,
+    "title": "delectus aut autem",
+    "completed": false
+}]
+
+
+
+
+
+
+const fetchTodos = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then( (response) => response.json())
+    .then( (json) => arrayOfTodos = json)
+}
+
+const logTodos = () => {
+    console.log(arrayOfTodos)
+
+    console.log(arrayOfTodos[0].userId) // => 14
+    console.log(arrayOfTodos[1].userId) // => 20
+}
+
+const populateTodos = () => {
+
+    const newListElement = document.createElement("li")
+    const newTextNode = document.createTextNode(arrayOfTodos[0].title)
+    newListElement.appendChild(newTextNode)
+
+    const olElement = document.getElementById("todo-list")
+    olElement.appendChild(newListElement)
+
+}
